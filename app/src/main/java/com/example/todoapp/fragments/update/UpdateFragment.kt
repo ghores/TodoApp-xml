2d62344dbb2f58c65f80a8234e41b23bea48c442
ghores <ghazimoradi.amin@gmail.com>
2025-10-09 18:34:23 +0330

    Implement Transition Animation between Fragments

--- app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/todoapp/fragments/update/UpdateFragment.kt
@@ -3,17 +3,33 @@ package com.example.todoapp.fragments.update
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.todoapp.R
+import com.example.todoapp.databinding.FragmentUpdateBinding
 
 class UpdateFragment : Fragment() {
+    private var _binding: FragmentUpdateBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_update, container, false)
+        _binding = FragmentUpdateBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Set menu
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 }
