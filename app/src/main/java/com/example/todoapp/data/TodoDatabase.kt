eb1d08657330cc729e2dc0478b8cb55ecb8c2db8
ghores <ghazimoradi.amin@gmail.com>
2025-10-09 18:57:36 +0330

    Implementation TodoDatabase

---
+++ app/src/main/java/com/example/todoapp/data/TodoDatabase.kt
@@ -0,0 +1,31 @@
+package com.example.todoapp.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [ToDoData::class], version = 1, exportSchema = false)
+abstract class TodoDatabase: RoomDatabase() {
+    abstract fun toDoDao(): ToDoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoDatabase? = null
+        fun getDatabase(context: Context): TodoDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TodoDatabase::class.java,
+                    "todo_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
